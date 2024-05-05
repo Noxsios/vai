f8ee36b31c87ee04abda2e1380a6db7a6676b162
razzle <harry@razzle.cloud>
2024-05-05 03:39:29 -0500

    reorg cmd
    Signed-off-by: razzle <harry@razzle.cloud>

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@
 package cmd
 
 import (
-	"os"
 	"runtime/debug"
 
 	"github.com/charmbracelet/log"
@@ -15,72 +14,65 @@ var level string
 var ver bool
 var list bool
 
-var rootCmd = &cobra.Command{
-	Use:   "vai",
-	Short: "A simple task runner",
-	PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
-		l, err := log.ParseLevel(level)
-		if err != nil {
-			return err
-		}
-		vai.SetLogLevel(l)
-		return nil
-	},
-	RunE: func(_ *cobra.Command, args []string) error {
-		logger := vai.Logger()
-
-		if ver {
-			bi, ok := debug.ReadBuildInfo()
-			if ok {
-				logger.Printf("%s", bi.Main.Version)
-			}
-			return nil
-		}
-
-		wf, err := vai.ReadAndValidate(vai.DefaultFileName)
-		if err != nil {
-			return err
-		}
-
-		if len(args) == 0 {
-			args = append(args, vai.DefaultTaskName)
-		}
-
-		with := make(vai.With)
-		for k, v := range w {
-			with[k] = v
-		}
-
-		if list {
-			logger.Print("Available:\n")
-			for k := range wf {
-				logger.Printf("- %s", k)
-			}
-			return nil
-		}
-		for _, call := range args {
-			if err := vai.Run(wf, call, with); err != nil {
+func NewRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vai",
+		Short: "A simple task runner",
+		PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
+			l, err := log.ParseLevel(level)
+			if err != nil {
 				return err
 			}
-		}
-		return nil
-	},
-}
+			vai.SetLogLevel(l)
+			return nil
+		},
+		SilenceUsage: true,
+		RunE: func(_ *cobra.Command, args []string) error {
+			logger := vai.Logger()
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	rootCmd.SilenceUsage = true
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+			if ver {
+				bi, ok := debug.ReadBuildInfo()
+				if ok {
+					logger.Printf("%s", bi.Main.Version)
+				}
+				return nil
+			}
+
+			wf, err := vai.ReadAndValidate(vai.DefaultFileName)
+			if err != nil {
+				return err
+			}
+
+			if len(args) == 0 {
+				args = append(args, vai.DefaultTaskName)
+			}
+
+			with := make(vai.With)
+			for k, v := range w {
+				with[k] = v
+			}
+
+			if list {
+				logger.Print("Available:\n")
+				for k := range wf {
+					logger.Printf("- %s", k)
+				}
+				return nil
+			}
+			for _, call := range args {
+				if err := vai.Run(wf, call, with); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
 	}
-}
 
-func init() {
-	rootCmd.Flags().StringToStringVarP(&w, "with", "w", nil, "variables to pass to the called task(s)")
-	rootCmd.Flags().StringVarP(&level, "log-level", "l", "info", "log level")
-	rootCmd.Flags().BoolVarP(&ver, "version", "V", false, "print version")
-	rootCmd.Flags().BoolVarP(&vai.Force, "force", "F", false, "ignore checksum mismatch for cached remote files")
-	rootCmd.Flags().BoolVar(&list, "list", false, "list available tasks")
+	cmd.Flags().StringToStringVarP(&w, "with", "w", nil, "variables to pass to the called task(s)")
+	cmd.Flags().StringVarP(&level, "log-level", "l", "info", "log level")
+	cmd.Flags().BoolVarP(&ver, "version", "V", false, "print version")
+	cmd.Flags().BoolVarP(&vai.Force, "force", "F", false, "ignore checksum mismatch for cached remote files")
+	cmd.Flags().BoolVar(&list, "list", false, "list available tasks")
+
+	return cmd
 }
