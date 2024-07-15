47fcec0cacc28f3f79e86d3e08e6d964155c3bcc
razzle <harry@razzle.cloud>
2024-07-15 11:42:36 -0500

    handle to string
    Signed-off-by: razzle <harry@razzle.cloud>

--- modv/semver.go
+++ modv/semver.go
@@ -15,6 +15,10 @@ var SemverModule = map[string]tengo.Object{
 		Name:  "new_version",
 		Value: semverNewVersion,
 	},
+	"to_string": &tengo.UserFunction{
+		Name:  "to_string",
+		Value: semverToString,
+	},
 }
 
 func semverNewVersion(args ...tengo.Object) (tengo.Object, error) {
@@ -36,6 +40,12 @@ func semverNewVersion(args ...tengo.Object) (tengo.Object, error) {
 		return nil, err
 	}
 
+	var prefix string
+	// logic copied from semver's originalVPrefix
+	if v.Original() != "" && v.Original()[0:1] == "v" {
+		prefix = "v"
+	}
+
 	return &tengo.Map{
 		Value: map[string]tengo.Object{
 			"major": &tengo.Int{
@@ -53,6 +63,90 @@ func semverNewVersion(args ...tengo.Object) (tengo.Object, error) {
 			"metadata": &tengo.String{
 				Value: v.Metadata(),
 			},
+			"prefix": &tengo.String{
+				Value: prefix,
+			},
 		},
+	}, nil
+}
+
+func semverToString(args ...tengo.Object) (tengo.Object, error) {
+	if len(args) != 1 {
+		return nil, tengo.ErrWrongNumArguments
+	}
+	m, ok := args[0].(*tengo.Map)
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "first",
+			Expected: "map",
+			Found:    args[0].TypeName(),
+		}
+	}
+
+	major, ok := (m.Value["major"].(*tengo.Int))
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "major",
+			Expected: "int",
+			Found:    m.Value["major"].TypeName(),
+		}
+	}
+
+	minor, ok := m.Value["minor"].(*tengo.Int)
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "minor",
+			Expected: "int",
+			Found:    m.Value["minor"].TypeName(),
+		}
+	}
+
+	patch, ok := m.Value["patch"].(*tengo.Int)
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "patch",
+			Expected: "int",
+			Found:    m.Value["patch"].TypeName(),
+		}
+	}
+
+	prerelease, ok := m.Value["prerelease"].(*tengo.String)
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "prerelease",
+			Expected: "string",
+			Found:    m.Value["prerelease"].TypeName(),
+		}
+	}
+
+	metadata, ok := m.Value["metadata"].(*tengo.String)
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "metadata",
+			Expected: "string",
+			Found:    m.Value["metadata"].TypeName(),
+		}
+	}
+
+	prefix, ok := (m.Value["prefix"].(*tengo.String))
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "prefix",
+			Expected: "string",
+			Found:    m.Value["prefix"].TypeName(),
+		}
+	}
+
+	if prefix.Value != "" && prefix.Value != "v" {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "prefix",
+			Expected: "literal 'v' or empty string",
+			Found:    prefix.Value,
+		}
+	}
+
+	v := semver.New(uint64(major.Value), uint64(minor.Value), uint64(patch.Value), prerelease.Value, metadata.Value)
+	return &tengo.String{
+		Value: prefix.Value + v.String(),
 	}, nil
 }
