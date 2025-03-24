6336bb7c7bdb1a99a514718a6d07aad4f229f88f
Harry Randazzo <harry@razzle.cloud>
2025-03-24 16:54:33 -0500

    fix tests
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- log_test.go
+++ log_test.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2024-Present Harry Randazzo
+// SPDX-FileCopyrightText: 2024-Present Defense Unicorns
 
-package vai
+package maru2
 
 import (
 	"context"
